df9b034e99d7c2b9b3edefa5d692e97c253cdb65
Jihoon Han <jihan2019@u.northwestern.edu>
2019-03-08 05:36:54 -0600

    more coding
    Setups and changes. Need to still work from process_wifi

--- wifi_firmware/src/wifi.c
+++ wifi_firmware/src/wifi.c
@@ -10,10 +10,12 @@
 
 volatile uint32_t received_byte_wifi = 0;
 volatile uint32_t input_pos_wifi = 0;
+volatile bool new_rx_wifi = false;
+volatile bool wifi_comm_success = false;
 
 
 // Handler for incoming data from the WIFI. Calls process_incoming_byte_wifi when a new byte arrives
-void wifi_usart_handler(void)															
+void WIFI_USART_HANDLER(void)														
 {
 	uint32_t ul_status;									// 8 byte variable for status
 
@@ -23,13 +25,14 @@ void wifi_usart_handler(void)
 	/* Receive buffer is full. */
 	if (ul_status & US_CSR_RXBUFF) {                    // Checking if we received a byte
 		usart_read(WIFI_USART, &received_byte_wifi);
+		new_rx_wifi = true;
 		process_incoming_byte_wifi((uint8_t)received_byte_wifi);
 	}
 }
 
 
 // Stores every incoming by (in_byte) from the AMW136 in a buffer
-static void process_incoming_byte_wifi(uint8_t in_byte)									
+void process_incoming_byte_wifi(uint8_t in_byte)									
 {
 	input_line_wifi[input_pos_wifi] = in_byte;            // Saves the incoming byte into a next position
 	input_pos_wifi = input_pos_wifi + 1;				  // Increments the position
@@ -37,20 +40,90 @@ static void process_incoming_byte_wifi(uint8_t in_byte)
 
 
 // Handler for "command complete" rising-edge interrupt from AMW136. Process the response of the AMW136
-static void wifi_command_response_handler(uint32_t ul_id, uint32_t ul_mask) {				// id of what is going on, mask of which pin what triggered the interrupt	
+void wifi_command_response_handler(uint32_t ul_id, uint32_t ul_mask)						// id of what is going on, mask of which pin what triggered the interrupt	
+{
 	unused(ul_id);				// unused is a silencer to avoid compiler warning
 	unused(ul_mask);
-
+	
+	wifi_comm_success = true;
 	process_data_wifi();
+	for (int jj=0;jj<MAX_INPUT_WIFI;jj++) input_line_wifi[jj] = 0;                          // Once the data was processed, clear the buffer
+	input_pos_wifi = 0;
 }
 
 
-
 // Processes the response of the AMW136, which should be stored in the buffer filled by process_incoming_byte_wifi. Looking for certain responses that the AMW136 gives
-static void process_data_wifi(void)
+void process_data_wifi(void)
 {
 		if (strstr(input_line_wifi, "")){
 			ioport_toggle_pin_level(PIN_LED);
 		}
 }
 
+
+void wifi_web_setup_handler(uint32_t ul_id, uint32_t ul_mask)
+{
+	
+	
+}
+
+
+// Configuration of USART port used to communicate with the AMW136.
+void configure_usart_wifi(void)
+{
+	gpio_configure_pin(PIN_USART0_RXD_IDX, PIN_USART0_RXD_FLAGS);
+	gpio_configure_pin(PIN_USART0_TXD_IDX, PIN_USART0_TXD_FLAGS);
+	gpio_configure_pin(PIN_USART0_CTS_IDX, PIN_USART0_CTS_FLAGS);
+	//gpio_configure_pin(PIN_USART0_RTS_IDX, PIN_USART0_RTS_FLAGS);
+	
+	static uint32_t ul_sysclk;
+	const sam_usart_opt_t usart_console_settings = {
+		WIFI_USART_BAUDRATE,
+		US_MR_CHRL_8_BIT,
+		US_MR_PAR_NO,
+		US_MR_NBSTOP_1_BIT,
+		US_MR_CHMODE_NORMAL,
+		/* This field is only used in IrDA mode. */
+		0
+	};
+
+	/* Get peripheral clock. */
+	ul_sysclk = sysclk_get_peripheral_hz();
+
+	/* Enable peripheral clock. */
+	sysclk_enable_peripheral_clock(WIFI_USART_ID);
+
+	/* Configure USART. */
+	usart_init_hw_handshaking(WIFI_USART, &usart_console_settings, ul_sysclk);
+
+	/* Disable all the interrupts. */
+	usart_disable_interrupt(WIFI_USART, ALL_INTERRUPT_MASK);
+	
+	/* Enable TX & RX function. */
+	usart_enable_tx(WIFI_USART);
+	usart_enable_rx(WIFI_USART);
+	
+	usart_enable_interrupt(WIFI_USART, US_IER_RXRDY);
+
+	/* Configure and enable interrupt of USART. */
+	NVIC_EnableIRQ(WIFI_USART_IRQn);
+}
+
+
+
+// Configuration of "command complete" rising-edge interrupt.
+void configure_wifi_comm_pin(void)
+{
+	/* Configure PIO clock. */
+	pmc_enable_periph_clk(WIFI_COMM_ID);
+
+	/* Initialize PIO interrupt handler, see PIO definition in conf_board.h
+	**/
+	pio_handler_set(WIFI_COMM_PIO, WIFI_COMM_ID, WIFI_COMM_PIN_NUM, WIFI_COMM_ATTR, wifi_command_response_handler);
+
+	/* Enable PIO controller IRQs. */
+	NVIC_EnableIRQ((IRQn_Type)WIFI_COMM_ID);
+
+	/* Enable PIO interrupt lines. */
+	pio_enable_interrupt(WIFI_COMM_PIO, WIFI_COMM_PIN_NUM);
+}
--- wifi_firmware/src/wifi.h
+++ wifi_firmware/src/wifi.h
@@ -15,19 +15,65 @@
 
 
 #define WIFI_USART                USART0
+#define WIFI_USART_BAUDRATE			115200
+#define WIFI_USART_ID				ID_USART0     // Peripheral id
+#define WIFI_USART_IRQn				USART0_IRQn	  // Interrupt 
+#define WIFI_USART_HANDLER			USART0_Handler
+
+
+// USART byte setup
+#define WIFI_USART_CHAR_LENGTH		US_MR_CHRL_8_BIT		// 8 bit long
+#define WIFI_USART_PARITY			US_MR_PAR_NO			// no parity bit
+#define WIFI_USART_STOP_BITS		US_MR_NBSTOP_1_BIT		// 1 stop bit
+#define WIFI_USART_MODE				US_MR_CHMODE_NORMAL		// normal mode
+
+
+/** USART0 pin RX */
+#define PIN_USART0_RXD	  {PIO_PA5A_RXD0, PIOA, ID_PIOA, PIO_PERIPH_A, PIO_PULLUP}
+#define PIN_USART0_RXD_IDX        (PIO_PA5_IDX)
+#define PIN_USART0_RXD_FLAGS      (PIO_PERIPH_A | PIO_PULLUP)
+/** USART0 pin TX */
+#define PIN_USART0_TXD    {PIO_PA6A_TXD0, PIOA, ID_PIOA, PIO_PERIPH_A, PIO_PULLUP}
+#define PIN_USART0_TXD_IDX        (PIO_PA6_IDX)
+#define PIN_USART0_TXD_FLAGS      (PIO_PERIPH_A | PIO_PULLUP)
+/** USART0 pin CTS */
+#define PIN_USART0_CTS    {PIO_PA8A_CTS0, PIOA, ID_PIOA, PIO_PERIPH_A, PIO_PULLUP}
+#define PIN_USART0_CTS_IDX        (PIO_PA8_IDX)
+#define PIN_USART0_CTS_FLAGS      (PIO_PERIPH_A | PIO_PULLUP)
+/** USART0 pin RTS */
+#define PIN_USART0_RTS    {PIO_PA7A_RTS0, PIOA, ID_PIOA, PIO_PERIPH_A, PIO_PULLUP}
+#define PIN_USART0_RTS_IDX        (PIO_PA7_IDX)
+#define PIN_USART0_RTS_FLAGS      (PIO_PERIPH_A | PIO_PULLUP)
+
+
+/** All interrupt mask. */
+#define ALL_INTERRUPT_MASK  0xffffffff
+
+
+
+// Pin setups, these should be changed depending on our setting
+#define WIFI_COMM_PIN_NUM			PIO_PB10
+#define WIFI_COMM_PIO				PIOB
+#define WIFI_COMM_ID				ID_PIOB
+#define WIFI_COMM_MASK				PIO_PB10_IDX
+#define WIFI_COMM_ATTR				PIO_IT_RISE_EDGE
 
 
 
 
-
-
+#define MAX_INPUT_WIFI 10000;
 volatile uint32_t received_byte_wifi;
 volatile uint32_t input_pos_wifi;
-volatile char input_line_wifi[1000];    // giving 1000 bytes so that buffer doesn't run over easily
+volatile bool wifi_comm_success;
+volatile bool new_rx_wifi;
+volatile char input_line_wifi[MAX_INPUT_WIFI];    // giving 10000 bytes so that buffer doesn't run over easily
 
 
-
-
-
+// void wifi_usart_handler(void);
+void process_incoming_byte_wifi(uint8_t in_byte);
+void wifi_command_response_handler(uint32_t ul_id, uint32_t ul_mask);
+void process_data_wifi(void);
+void wifi_web_setup_handler(uint32_t ul_id, uint32_t ul_mask);
+void configure_wifi_comm_pin(void);
 
 #endif /* WIFI_H_ */
