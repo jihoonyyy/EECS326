3ea5cf4d172ce0bb819d441708af6477a48bb5fd
ece-lab3 <ece-lab3@pclabs.ece.northwestern.edu>
2019-03-17 00:14:41 -0500

    wengqekljrgnl

--- wifi_firmware/src/config/conf_board.h
+++ wifi_firmware/src/config/conf_board.h
@@ -37,11 +37,11 @@
 
 
 
-
-#define WIFI_CTS_PIN		PIO_PA7_IDX
+#define OV2640_RESET_PIN    PIO_PA20_IDX
+#define WIFI_CTS_PIN		PIO_PA8_IDX
 #define WIFI_RESET_PIN      PIO_PB3_IDX
 #define WIFI_WEB_SETUP_PIN  PIO_PB4_IDX          // Used PB4 for setup pin
-
-
+#define WIFI_COMM_PIN		PIO_PB10_IDX
+// #define WIFI_
 
 #endif // CONF_BOARD_H
